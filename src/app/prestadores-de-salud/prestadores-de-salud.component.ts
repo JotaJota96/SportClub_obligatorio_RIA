374b92f3dc52dabc1d57c0c49f9ce57ee0ff6738
alfajor <alfa@gmail.com>
2020-06-03 17:26:03 -0300

    en prestadores de salu, el modal agregar y modificar se muestran correctamente

--- src/app/prestadores-de-salud/prestadores-de-salud.component.ts
+++ src/app/prestadores-de-salud/prestadores-de-salud.component.ts
@@ -7,6 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrestadoresDeSaludComponent implements OnInit {
 
+  isModalVisible:boolean = false; //se muestra o no el modal
+  accionAgregar:boolean = true; //si esta en true es agregar, si esta en false es modificar
+
+  titulo:string ="";
+
+  deleteOrder() {
+    console.log("deleteado---------------------");
+  }
+
+  cerrar(){
+    this.isModalVisible = false;
+  }
+
+  abrirModificar(indice:number){ //indice en el array del elemento que se quiere modificar
+    this.titulo="Modificar";
+    //carga los datos correspondientes a esa fila
+    this.accionAgregar = false;
+    this.isModalVisible = true;//muestra el modal.
+  }
+
+  abrirAgregar(){
+    this.titulo="Agregar";
+    //muestra el modal
+    this.accionAgregar = true;
+    this.isModalVisible = true;
+  }
+
+  borrar(){
+    //borra la fila
+  }
+
+  confirmar(){
+    //borra la fila
+    if(this.accionAgregar){
+      //agregar
+    }else{
+      //modificar
+    }
+    this.cerrar();//cierra el modal
+  }
+
   constructor() { }
 
   ngOnInit(): void {
