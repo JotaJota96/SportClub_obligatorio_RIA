0f0c22644f3e624f31a79cb6670dbdc06d37871c
alfajor <alfa@gmail.com>
2020-06-05 02:32:55 -0300

    casi Hecho CRUD precio

--- src/app/precios/precios.component.ts
+++ src/app/precios/precios.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Precio } from '../clases/precio';
+import { PreciosService } from '../servicios/precios.service';
+import { CategoriasService } from '../servicios/categorias.service';
+import { Categoria } from '../clases/categoria';
 
 @Component({
   selector: 'app-precios',
@@ -7,32 +11,48 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./precios.component.css']
 })
 export class PreciosComponent implements OnInit {
-
+  liscaCateg:Categoria[];
   isModalVisible:boolean = false; //se muestra o no el modal
   accionAgregar:boolean = true; //si esta en true es agregar, si esta en false es modificar
+
+  listaPrecios:Precio[];
 
   titulo:string ="";
 
   public profileForm: FormGroup;
 
-  constructor() { }
+  constructor(protected precioService : PreciosService, protected catService : CategoriasService) { }
 
   ngOnInit(): void {
     this.profileForm = new FormGroup({
       valor: new FormControl('', [Validators.required, Validators.minLength(1),Validators.maxLength(10)]),
-      fechaDeVigencia: new FormControl('', [Validators.required, Validators.minLength(10),Validators.maxLength(10)]),
-      categoria: new FormControl('', [Validators.required, Validators.minLength(2),Validators.maxLength(50)]),
+      fechaVigencia: new FormControl('', [Validators.required, Validators.minLength(10),Validators.maxLength(10)]),
+      categoria: new FormControl(''),
+      id: new FormControl('')
     });
+    this.cargarLista();
+    this.cargarCategoria();
   }
+
+  cargarLista(){
+    this.precioService.getAll().subscribe(
+      (pre)=>{
+        this.listaPrecios=pre;
+      }
+    );
+  }
+
   cerrar(){
-    this.vaciarCampos();
     this.isModalVisible = false;
+    this.vaciarCampos();
   }
 
   abrirModificar(indice:number){ //indice en el array del elemento que se quiere modificar
-    this.vaciarCampos();
     this.titulo="Modificar";
-    //carga los datos correspondientes a esa fila
+    this.profileForm.controls['valor'].setValue(this.listaPrecios[indice].valor);
+    this.profileForm.controls['fechaVigencia'].setValue(this.listaPrecios[indice].fechaVigencia);
+    this.profileForm.controls['categoria'].setValue(this.listaPrecios[indice].categoria);
+    this.profileForm.controls['id'].setValue(this.listaPrecios[indice].id);
     this.accionAgregar = false;
     this.isModalVisible = true;//muestra el modal.
   }
@@ -44,26 +64,79 @@ export class PreciosComponent implements OnInit {
     this.isModalVisible = true;//abre el modal
   }
 
-  borrar(){
-    this.vaciarCampos();
-    //borra la fila
+  borrar(indice:number){
+    let id = this.listaPrecios[indice].id;
+    this.precioService.delete(id).subscribe(
+      (retorno)=>{
+        //hacer algo si login es correcto
+        alert("Se ha eliminado exitosamente");
+        this.cargarLista();
+      },
+      (error)=>{
+        alert("Ha ocurrido un error durante la eliminacion");
+      }
+    );
   }
 
   confirmar(){
-    this.vaciarCampos();
     //borra la fila
     if(this.accionAgregar){
-      //agregar
+      this.agregar();
     }else{
-      //modificar
+      this.modificar();
     }
     this.cerrar();//cierra el modal
   }
 
   vaciarCampos(){
     this.profileForm.controls['valor'].setValue("");
-    this.profileForm.controls['fechaDeVigencia'].setValue("");
+    this.profileForm.controls['fechaVigencia'].setValue("");
     this.profileForm.controls['categoria'].setValue("");
-    
+    this.profileForm.controls['id'].setValue("");
   }
+
+  agregar(){
+    let valor = this.profileForm.controls['valor'].value;
+    let fechaVigencia = this.profileForm.controls['fechaVigencia'].value;
+    let indiceCat = this.profileForm.controls['categoria'].value;
+    let datos = new Precio(0, valor, fechaVigencia, this.liscaCateg[indiceCat]);
+
+    this.precioService.create(datos).subscribe(
+      (retorno)=>{
+        //hacer algo si login es correcto
+        alert("Se ha agregado exitosamente");
+        this.cargarLista();
+      },
+      (error)=>{
+        alert("Ha ocurrido un error durante el agregado");
+      }
+    );
+  }
+
+  modificar(){
+    let valor = this.profileForm.controls['valor'].value;
+    let fechaVigencia = this.profileForm.controls['fechaVigencia'].value;
+    let indiceCat = this.profileForm.controls['categoria'].value;
+    let id = this.profileForm.controls['id'].value;
+    let datos = new Precio(id, valor, fechaVigencia, this.liscaCateg[indiceCat]);
+    this.precioService.edit(datos).subscribe(
+      (retorno)=>{
+        //hacer algo si login es correcto
+        alert("Se ha modificado exitosamente");
+        this.cargarLista();
+      },
+      (error)=>{
+        alert("Ha ocurrido un error durante la modificacion");
+      }
+    );
+  }
+
+  cargarCategoria(){
+    this.catService.getAll().subscribe(
+      (cats)=>{
+        this.liscaCateg =cats;
+      }
+    );
+  }
+
 }
