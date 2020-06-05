3426ca4852416343aa2ad6fb0b915f4dd10e5ea0
JotaJota96 <jjap96@gmail.com>
2020-06-05 15:08:10 -0300

    hecho CRUD Noticias, FALTA manejo de imagenes

--- src/app/noticias/noticias.component.ts
+++ src/app/noticias/noticias.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Noticia } from '../clases/noticia';
+import { NoticiasService } from '../servicios/noticias.service';
 
 @Component({
   selector: 'app-noticias',
@@ -14,7 +15,7 @@ export class NoticiasComponent implements OnInit {
   lstNoticias:Noticia[];
   public profileForm: FormGroup;
 
-  constructor() {
+  constructor(protected notiServ:NoticiasService) {
 
   }
 
@@ -22,20 +23,19 @@ export class NoticiasComponent implements OnInit {
     this.profileForm = new FormGroup({
       id: new FormControl(''),
       titulo: new FormControl('', [Validators.required, Validators.minLength(2),Validators.maxLength(50)]),
-      descripcion: new FormControl('', [Validators.required, Validators.minLength(2),Validators.maxLength(1000)]),
+      descripcion: new FormControl('', [Validators.required, Validators.minLength(2),Validators.maxLength(2000)]),
       imagen: new FormControl('', [Validators.required]),
       fechaCaducidad: new FormControl('', [Validators.required])
     });
+    this.cargarLista();
   }
 
   cargarLista(){
-    /*
-    this.convServ.getAll().subscribe(
-      (mdp)=>{
-        this.lstConvenios=mdp;
+    this.notiServ.getAll().subscribe(
+      (lst)=>{
+        this.lstNoticias = lst;
       }
     );
-    */
   }
 
   cerrar(){
@@ -46,6 +46,13 @@ export class NoticiasComponent implements OnInit {
   abrirModificar(indice:number){ //indice en el array del elemento que se quiere modificar
     this.titulo="Modificar";
 
+    this.profileForm.controls['id'].setValue(this.lstNoticias[indice].id);
+    this.profileForm.controls['titulo'].setValue(this.lstNoticias[indice].titulo);
+    this.profileForm.controls['descripcion'].setValue(this.lstNoticias[indice].descripcion);
+    this.profileForm.controls['imagen'].setValue(this.lstNoticias[indice].imagen);
+    this.profileForm.controls['fechaCaducidad'].setValue(this.lstNoticias[indice].fechaCaducidad);
+
+    console.log(indice);
     this.accionAgregar = false;
     this.isModalVisible = true;//muestra el modal.
   }
@@ -58,7 +65,17 @@ export class NoticiasComponent implements OnInit {
   }
 
   borrar(indice:number){
-
+    let id = this.lstNoticias[indice].id;
+    this.notiServ.delete(id).subscribe(
+      (retorno)=>{
+        //hacer algo si login es correcto
+        alert("Se ha eliminado exitosamente");
+        this.cargarLista();
+      },
+      (error)=>{
+        alert("Ha ocurrido un error durante la eliminacion");
+      }
+    );
   }
 
   confirmar(){
@@ -72,18 +89,49 @@ export class NoticiasComponent implements OnInit {
   }
 
   agregar(){
+    let titulo = this.profileForm.controls['titulo'].value;
+    let descripcion = this.profileForm.controls['descripcion'].value;
+    let imagen = this.profileForm.controls['imagen'].value;
+    let fechaCaducidad = this.profileForm.controls['fechaCaducidad'].value;
 
+    let datos = new Noticia(0, titulo, descripcion, imagen, fechaCaducidad);
+    this.notiServ.create(datos).subscribe(
+      (retorno)=>{
+        //hacer algo si login es correcto
+        alert("Se ha agregado exitosamente");
+        this.cargarLista();
+      },
+      (error)=>{
+        alert("Ha ocurrido un error durante el agregado");
+      }
+    );
   }
 
   modificar(){
+    let id = this.profileForm.controls['id'].value;
+    let titulo = this.profileForm.controls['titulo'].value;
+    let descripcion = this.profileForm.controls['descripcion'].value;
+    let imagen = this.profileForm.controls['imagen'].value;
+    let fechaCaducidad = this.profileForm.controls['fechaCaducidad'].value;
 
+    let datos = new Noticia(id, titulo, descripcion, imagen, fechaCaducidad);
+    this.notiServ.edit(datos).subscribe(
+      (retorno)=>{
+        //hacer algo si login es correcto
+        alert("Se ha agregado exitosamente");
+        this.cargarLista();
+      },
+      (error)=>{
+        alert("Ha ocurrido un error durante el agregado");
+      }
+    );
   }
 
   vaciarCampos(){
     this.profileForm.controls['id'].setValue("");
     this.profileForm.controls['titulo'].setValue("");
     this.profileForm.controls['descripcion'].setValue("");
-    this.profileForm.controls['imagen'].setValue("");
+    this.profileForm.controls['imagen'].setValue(" ");
     this.profileForm.controls['fechaCaducidad'].setValue("");
   }
 
